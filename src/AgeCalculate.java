e552ef84a3abfbb52c2c8a944c26cd5fd5a1de7e
mvaishnavi <vaishnavi4@gmail.com>
2015-11-12 00:31:41 +0530

    Computing age through seperate method. Added Method to check if birthyear is leap year.

--- src/AgeCalculate.java
+++ src/AgeCalculate.java
@@ -10,13 +10,29 @@ public class AgeCalculate {
             System.out.print(dateSplit[i]);
         int currentYear=2015;
         int birthYear= Integer.parseInt(dateSplit[2]);
+        computeAge(currentYear,birthYear);
+        int leap= calculateLeapYear(birthYear);
+        if(leap==1)
+            System.out.println("BirthYear is a Leap year");
+        else
+            System.out.println("Birthyear is not a leap year.");
+    }
 
-
-
+    private static void computeAge(int currentYear, int birthYear ) {
         int age= currentYear- birthYear;
         System.out.println("The current age of dob=31.8.1993 is" + age);
-
     }
-   
+
+    public static int calculateLeapYear(int birthYear) {
+        if(birthYear%4 == 0 && birthYear % 400 ==0)
+        {
+            int ret = 1;
+            return (ret);
+        }
+        else {
+            return (0);
+        }
+    }
+
 
 }
